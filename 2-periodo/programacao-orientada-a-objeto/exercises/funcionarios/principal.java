c44f72b1365db0c1362c885e628cd8c09137aa50
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-08 21:41:49 -0300

    Initialize Funcionario objects with details

--- 2-periodo/programacao-orientada-a-objeto/exercises/funcionarios/principal.java
+++ 2-periodo/programacao-orientada-a-objeto/exercises/funcionarios/principal.java
@@ -3,11 +3,28 @@ public class Principal {
         
         Funcionario func1 = new Funcionario();
         Funcionario func2 = new Funcionario();
+            
+        func1.nome = "Raquel Vitoria";
+        func1.cpf = "123.456.789-00";
+        func1.cargo = "Analista de Sistemas";
+        func1.salarioMensal = 3000.00;
+        func1.departamento = "Tecnologia";
 
-        func1.Nome = ;
-        func1.cpf = ;
-        func1.cargo = ;
-        func1.salarioMensal = ;
-        func1.departamento = ;
+        func1.Nome = "Lana Silva";
+        func1.cpf = "987.654.321-00";
+        func1.cargo = "Assistente";
+        func1.salarioMensal = 2500.00;
+        func1.departamento = "Administrativo";
+
+        func1.aplicarAumento(10);
+        func2.aplicarAumento(5);
+
+        System.out.println(func1.nome + " recebe R$ "
+                           + func1.calcularSalarioAnual()
+                           + " por ano.");
+
+        System.out.println(func2.nome + " recebe R$ "
+                           + func2.calcularSalarioAnual()
+                           + "por ano.");
     }
 }
