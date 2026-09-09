d7983ee73c29213d9669b1c2a665cb5d878ca1f2
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-08 22:39:02 -0300

    Initialize Televisor objects and set properties

--- 2-periodo/programacao-orientada-a-objeto/exercises/televisor/principal.java
+++ 2-periodo/programacao-orientada-a-objeto/exercises/televisor/principal.java
@@ -1,1 +1,25 @@
+public class Principal {
+  public static void main(String[] args) {
+    
+Televisor tv1 = new Televisor();
+Televisor tv2 = new Televisor();
 
+tv1.marca = "Samsung";
+tv1.polegada = 50;
+tv1.volume = 20;
+tv1.canalAtual = 5;
+tv1.ligada = false;
+
+tv2.marca = "LG";
+tv2.polegada = 43;
+tv2.volume = 15;
+tv2.canalAtual = 10;
+tv2.ligada = false;
+
+tv1.ligar();
+tv1.trocarCanal(8);
+
+tv2.ligar();
+tv2.trocarCanal(12);
+}
+}
