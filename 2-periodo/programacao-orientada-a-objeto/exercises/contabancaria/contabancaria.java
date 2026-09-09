7c5ad4d760c20299ce0ab4c0d616510a9c1e8f4c
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-09 08:13:54 -0300

    Update contabancaria.java

--- 2-periodo/programacao-orientada-a-objeto/exercises/contabancaria/contabancaria.java
+++ 2-periodo/programacao-orientada-a-objeto/exercises/contabancaria/contabancaria.java
@@ -1,3 +1,41 @@
+ public class ContaBancaria {
+     
+    private int numConta, agencia;
+    public String titular, tipoConta;
+    protected double saldo = 0.00;
+    public final double TAXA_MANUTENCAO_CONTA = 5.00;
+
+    public ContaBancaria(int numConta, int agencia, String titular, String tipoConta, double saldo){
+
+        this.numConta = numConta;
+        this.agencia = agencia;
+        this.titular = titular;
+        this.tipoConta = tipoConta;
+        this.saldo = 0.00;
+    }
+
+    public int getNumConta(){
+        return numConta;
+    }
+
+    public void setNunConta(int numConta){
+        this.numConta = numConta;
+    }
+
+    public void depositar(double valor){
+        saldo += valor;
+    }
+
+    public void sacar(double valor){
+        saldo -= valor;
+    }
+
+    public double consultarSaldo(){
+        saldo = saldo - TAXA_MANUTENCAO_CONTA;
+        return saldo;
+    }
+}
+
 /* public class ContaBancaria {
     int numConta, agencia;
     String titular, tipoConta;
@@ -42,43 +80,3 @@
         return saldo;
     }
 } */
-
----
-
-    public class ContaBancaria {
-    private int numConta, agencia;
-    public String titular, tipoConta;
-    protected double saldo = 0.00;
-    public final double TAXA_MANUTENCAO_CONTA = 5.00;
-
-    public ContaBancaria(int numConta, int agencia, String titular, String tipoConta, double saldo){
-
-        this.numConta = numConta;
-        this.agencia = agencia;
-        this.titular = titular;
-        this.tipoConta = tipoConta;
-        this.saldo = 0.00;
-    }
-
-    public int getNumConta(){
-        return numConta;
-    }
-
-    public void setNunConta(int numConta){
-        this.numConta = numConta;
-    }
-
-    public void depositar(double valor){
-        saldo += valor;
-    }
-
-    public void sacar(double valor){
-        saldo -= valor;
-    }
-
-    public double consultarSaldo(){
-        saldo = saldo - TAXA_MANUTENCAO_CONTA;
-        return saldo;
-    }
-}
-
