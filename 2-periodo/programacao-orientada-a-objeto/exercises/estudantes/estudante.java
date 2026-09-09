d18e083710b7b3065906c719e4d3493495f1c6f4
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-08 22:13:18 -0300

    Update estudante.java

--- 2-periodo/programacao-orientada-a-objeto/exercises/estudantes/estudante.java
+++ 2-periodo/programacao-orientada-a-objeto/exercises/estudantes/estudante.java
@@ -1,1 +1,19 @@
+public class Estudante {
+  String nome;
+  String matricula;
+  String curso;
+  int idade;
+  double notaMedia;
 
+  public void registrarNota(double valor) {
+    notaMedia = valor;
+  }
+
+  public String verificarSituacao() {
+    if (notaMedia >= 7) {
+      return "Aprovado";
+    } else {
+      return "Reprovado";
+    }
+  }
+}
