28a29e83e32348aa18c4cbb8c54a60716c84b2cb
Raquel <raquel.vitoria@a.ficr.edu.br>
2026-09-08 22:31:45 -0300

    Update televisor.java

--- 2-periodo/programacao-orientada-a-objeto/exercises/televisor/televisor.java
+++ 2-periodo/programacao-orientada-a-objeto/exercises/televisor/televisor.java
@@ -1,1 +1,22 @@
+public class Televisor {
 
+  String marca;
+  int polegada;
+  int volume;
+  int canalAtual;
+  boolean ligada;
+
+  public void ligar() {
+    ligada = true;
+    System.out.println("Televisor ligado.");
+  }
+
+  public void trocarCanal(int novoCanal) {
+    if (ligada) {
+      canalAtual = novoCanal;
+      System.out.println("Canal alterado para " + canalAtual);
+    } else {
+      System.out.println("Nao e possivel trocar o canal. A TV esta desligada.");
+    }
+  }
+}
